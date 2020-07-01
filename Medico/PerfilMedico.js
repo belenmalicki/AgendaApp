ace68fc6a0e96a72a5850290db8b77dae88a6673
Matías Panza <panzamatiasezequiel@gmail.com>
2020-07-01 02:33:33 -0300

    algunos cambios visuales, conexion de medico casi terminada

--- Medico/PerfilMedico.js
+++ Medico/PerfilMedico.js
@@ -4,6 +4,7 @@ import {Footer, FooterTab, Container, Col, Row} from 'native-base'
 import { Divider } from 'react-native-elements' 
 import { Ionicons } from '@expo/vector-icons';
 import AsyncStorage from '@react-native-community/async-storage'
+import utils from '../utils/utils';
 const { width } = Dimensions.get('window');
 
 
@@ -48,7 +49,6 @@ export default class Perfil extends Component {
     } else {
       const {usuario} = this.state;
       const dr = usuario.genero === 'femenino' ? 'DRA.' : 'DR.';
-      //console.log(usuario)
       return (
         <ScrollView style={{ flex: 1 }}>
 
@@ -61,11 +61,11 @@ export default class Perfil extends Component {
               <View style={{ flexDirection: 'row', marginBottom: 10, justifyContent: "space-around", }}>
                 <View style={{ alignItems: "center" }}>
                   <Text style={{ fontSize: 12, marginTop: 12 }}>Nro. Socio/a</Text>
-                  <Text style={{ fontSize: 12, marginTop: 5, fontWeight: 'bold' }}>{usuario.nro_socio}</Text>
+                  <Text style={{ fontSize: 12, marginTop: 5, fontWeight: 'bold' }}>{utils.separarEnMiles(usuario.nro_socio)}</Text>
                 </View>
                 <View style={{ alignItems: "center" }}>
                   <Text style={{ fontSize: 12, marginTop: 12 }}>Matrícula</Text>
-                  <Text style={{ fontSize: 12, marginTop: 5, fontWeight: 'bold' }}>{usuario.medico.nro_matricula}</Text>
+                  <Text style={{ fontSize: 12, marginTop: 5, fontWeight: 'bold' }}>{utils.separarEnMiles(usuario.medico.nro_matricula)}</Text>
                 </View>
               </View>
             </View>
@@ -81,7 +81,7 @@ export default class Perfil extends Component {
             DNI
       </Text>
           <Text style={{ fontSize: 13, lineHeight: 16, marginHorizontal: '6%', color: 'grey' }}>
-            {usuario.dni}
+            {utils.separarEnMiles(usuario.dni)}
       </Text>
           <Divider style={{ backgroundColor: 'black', marginHorizontal: '5%' }} />
           <Text style={{ marginTop: '5%', textAlign: 'justify', fontSize: 14, marginBottom: 15, lineHeight: 16, marginHorizontal: '5%' }}>
